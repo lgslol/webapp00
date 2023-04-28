35a5d9c91fb07b47e38d418e809ce5a4d62cb82d
lgslol <132074485+lgslol@users.noreply.github.com>
2023-04-28 11:11:49 -0300

    webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -15,3 +15,13 @@ st.write("0 recuperações")
 st.write("Melhor que o cecconello no enem de Mat")
 
 st.write("Progamador profissional desde 28/04/2023")
+
+# pede ao usuário um número inteiro
+numero = int(input("Digite um número inteiro: "))
+
+# verifica se o número é par ou ímpar
+if numero % 2 == 0:
+    print(numero, "é par")
+else:
+    print(numero, "é ímpar")
+
