8289019077db2c96044c2762e41067c157545bd9
lgslol <132074485+lgslol@users.noreply.github.com>
2023-04-28 11:07:55 -0300

    webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -4,7 +4,7 @@
 import streamlit as st
 
 # Use st.title("") para adicionar um TÍTULO ao seu Web app
-st.title("Lezn3Kpa
+st.title("Leozn3Kpa")
 
 # Use st.header("") para adicionar um CABEÇALHO ao seu Web app
 st.header("O mestre do AZ")
