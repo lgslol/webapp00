e7bf2eb47e9fe16d923f7338a71832ff3b38a92a
lgslol <132074485+lgslol@users.noreply.github.com>
2023-04-28 11:13:37 -0300

    webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -1,21 +1,3 @@
-# myFirstStreamlitApp.py
-  
-#import the library
-import streamlit as st
-
-# Use st.title("") para adicionar um TÍTULO ao seu Web app
-st.title("Leozn3Kpa")
-
-# Use st.header("") para adicionar um CABEÇALHO ao seu Web app
-st.header("O mestre do AZ")
-         
-# Use st.write("") para adicionar um texto ao seu Web app
-st.write("0 recuperações")
-
-st.write("Melhor que o cecconello no enem de Mat")
-
-st.write("Progamador profissional desde 28/04/2023")
-
 # solicita ao usuário um número inteiro
 numero = int(input("Digite um número inteiro: "))
 
